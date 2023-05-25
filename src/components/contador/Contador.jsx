954dff0d29aa4c131e2da7284ee0ed5b9bba10cf
Diego Ronan Kulian <dkulian@veepee.com>
2023-05-25 19:08:35 +0200

    dividindo para varios componentes e trabalhando com comunicacao direta (pai > filho) e indireta (filho < pai)

--- src/components/contador/Contador.jsx
+++ src/components/contador/Contador.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import "./Contador.css";
+import Display from "./Display";
+import PassoForm from "./PassoForm";
+import Botoes from "./Botoes";
 export default class Contador extends Component {
         
     state = {
@@ -19,9 +22,9 @@ export default class Contador extends Component {
         })
     }
 
-    setPasso = (ev) => {
+    setPasso = (novoPasso) => {
         this.setState({
-            passo: +ev.target.value // o + converte para inteiro
+            passo: novoPasso,
         })
     }
 
@@ -30,17 +33,9 @@ export default class Contador extends Component {
         return (
             <div className="Contador"> 
                 <h2>Contador</h2>
-                <h3>{this.state.numero}</h3>
-                <div>
-                <label htmlFor="passoInput">Passo </label>
-                <input 
-                    id="passoInput" 
-                    type="number" 
-                    value={this.state.passo} 
-                    onChange={this.setPasso} />
-                </div>
-                <button onClick={this.inc}>+</button>
-                <button onClick={this.dec}>-</button>
+                <Display numero={this.state.numero} />
+                <PassoForm passo={this.state.passo} setPasso={this.setPasso}/>
+                <Botoes setIncrementar={this.inc} setDecrementar={this.dec}/>
             </div>
         )
 
